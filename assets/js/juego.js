e1fc89177789031593d02c7412c272d01a0a309b
Chech3 <joseangelgarces123@gmail.com>
2024-06-27 12:41:57 -0400

    Agregada funcion que genera nuevo deck

---
+++ assets/js/juego.js
@@ -0,0 +1,28 @@
+/* 2C = Two of Clubs
+2C = Two of Diamonds
+2C = Two of Hearts
+2C = Two of Spades */
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K']
+
+
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (const tipo of tipos) {
+            deck.push(i + tipo)
+        }
+    }
+    for (const tipo of tipos) {
+        for (const esp of especiales) {
+            deck.push(esp + tipo);
+        }
+    }
+    deck = _.shuffle(deck);
+
+    return deck;
+}
+
+console.log(crearDeck());
+
